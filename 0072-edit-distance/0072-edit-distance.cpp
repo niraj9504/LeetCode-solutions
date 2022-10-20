62c58be10d833344b7e72d42db5b8862b5acfa82
Niraj Kumar <86815759+niraj9504@users.noreply.github.com>
2022-10-20 12:43:39 +0530

    Time: 11 ms (89.14%), Space: 7.2 MB (88.70%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minDistance(string s1, string s2) {
+        int n=s1.size(),m=s2.size();
+        int dp[n+1][m+1];
+        //directly counting min of three opern. and skiping common
+        for(int i=0;i<=n;i++)dp[i][0]=i;
+        for(int j=0;j<=m;j++)dp[0][j]=j;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s1[i-1]==s2[j-1]){
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=min(dp[i-1][j],min(dp[i][j-1],dp[i-1][j-1]))+1;
+                }
+            }
+        }
+        return dp[n][m];
+    }
+};
