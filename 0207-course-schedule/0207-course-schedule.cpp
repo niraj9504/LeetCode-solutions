27d4c868728c68414a32247035545576fac2d03c
Niraj Kumar <86815759+niraj9504@users.noreply.github.com>
2022-10-22 18:49:50 +0530

    Time: 58 ms (21.54%), Space: 13.8 MB (60.69%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,30 +1,33 @@
 class Solution {
 public:
-    bool dfs(int node,vector<int> &vis,vector<int> &dfsvis,vector<int> adj[]){
-        vis[node]=1;
-        dfsvis[node]=1;
-        for(auto it:adj[node]){
-            if(!vis[it]){
-                if(dfs(it,vis,dfsvis,adj))return true;
-            }
-            else if(dfsvis[it]){
-                return true;
-            }
-        }dfsvis[node]=0;
-        return false;
-    }
     bool canFinish(int n, vector<vector<int>>& graph) {
         vector<int> adj[n];
         for(auto it:graph){
             int u=it[1],v=it[0];
             adj[u].push_back(v);
         }
-        vector<int> vis(n,0),dfsvis(n,0);
+        vector<int> indegree(n);
         for(int i=0;i<n;i++){
-            if(!vis[i]){
-                if(dfs(i,vis,dfsvis,adj))return false;
+            for(auto it:adj[i]){
+                indegree[it]++;
             }
         }
-        return true;
+        queue<int> q;
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0)q.push(i);
+        }
+        int count=0;
+        while(!q.empty()){
+            int x=q.front();q.pop();
+            count++;
+            for(auto it:adj[x]){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.push(it);
+                }
+            }
+        }
+        if(count==n)return true;
+        return false;
     }
 };
