95b714753a1b72339a0a18ad7e5112dafd04dbd7
Niraj Kumar <86815759+niraj9504@users.noreply.github.com>
2022-10-23 10:08:26 +0530

    Time: 260 ms (61.54%), Space: 76.8 MB (100.00%) - LeetHub

---
+++ 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    using ll=long long;
+    ll paisa(ll m,vector<int> &nums,vector<int> &cost){
+        ll kitna=0,n=nums.size();
+        for(int i=0;i<n;i++){
+            kitna+=(ll)(cost[i]*(abs(nums[i]-m)));
+        }
+        return kitna;
+    }
+    long long minCost(vector<int>& nums, vector<int>& cost) {
+        ll l=*min_element(nums.begin(),nums.end());
+        ll h=*max_element(nums.begin(),nums.end());
+        while(l<h){
+            ll mid=(l+h)/2;
+            ll a=paisa(mid,nums,cost);
+            ll b=paisa(mid+1,nums,cost);
+            if(a>b){
+                l=mid+1;
+            }
+            else{
+                h=mid;
+            }
+        }
+        return paisa(l,nums,cost);
+    }
+};
