0d3aeb8b32431914b5e06393ab45f2e46ba2be7d
Niraj Kumar <86815759+niraj9504@users.noreply.github.com>
2022-10-23 10:25:43 +0530

    Time: 771 ms (15.38%), Space: 77 MB (76.92%) - LeetHub

--- 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.cpp
+++ 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.cpp
@@ -11,17 +11,19 @@ public:
     long long minCost(vector<int>& nums, vector<int>& cost) {
         ll l=*min_element(nums.begin(),nums.end());
         ll h=*max_element(nums.begin(),nums.end());
-        while(l<h){
+        ll ans=LONG_MAX;
+        while(l<=h){
             ll mid=(l+h)/2;
-            ll a=paisa(mid,nums,cost);
+            ll a=paisa(mid-1,nums,cost);
             ll b=paisa(mid+1,nums,cost);
             if(a>b){
                 l=mid+1;
             }
             else{
-                h=mid;
+                h=mid-1;
             }
+            ans=min(ans,paisa(mid,nums,cost));
         }
-        return paisa(l,nums,cost);
+        return ans;
     }
 };
