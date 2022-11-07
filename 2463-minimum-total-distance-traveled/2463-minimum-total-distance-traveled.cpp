d1ff5ed078de348217eee3006ef84a590ce22e45
Niraj Kumar <86815759+niraj9504@users.noreply.github.com>
2022-11-07 23:17:44 +0530

    Time: 276 ms (50.00%), Space: 19.3 MB (75.00%) - LeetHub

---
+++ 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    using ll=long long;
+    ll dp[101][10001];
+    long long solve(int i,int j,vector<int> &robot,vector<int> &posn){
+        if(i==robot.size())return 0;
+        if(j==posn.size())return 1e16;
+        if(dp[i][j]!=-1)return dp[i][j];
+        ll pick=solve(i+1,j+1,robot,posn)+abs(posn[j]-robot[i]);
+        ll not_pick=solve(i,j+1,robot,posn);
+        return dp[i][j]=min(pick,not_pick);
+    }
+    long long minimumTotalDistance(vector<int>& robot, vector<vector<int>>& fac) {
+        vector<int> posn;
+        memset(dp,-1,sizeof(dp));
+        sort(robot.begin(),robot.end());
+        sort(fac.begin(),fac.end());
+        for(auto it:fac){
+            int x=it[0],y=it[1];
+            for(int i=1;i<=y;i++){
+                posn.push_back(x);
+            }
+        }
+        return solve(0,0,robot,posn);
+    }
+};
