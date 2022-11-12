b858c9a5eb25fbbf152517c6916c4b94694f59cd
Niraj Kumar <86815759+niraj9504@users.noreply.github.com>
2022-11-12 20:36:15 +0530

    Time: 4 ms (81.01%), Space: 10.1 MB (81.92%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -1,22 +1,11 @@
 class Solution {
 public:
-    string ans;
-    void solve(int i,int n,unordered_set<string> &map,string curr,bool flag){
-        if(flag)return;
-        if(i==n){
-            if(map.find(curr)==map.end()){
-                flag=true;
-                ans=curr;
-            }
-            return;
-        }
-        solve(i+1,n,map,curr+'0',flag);
-        solve(i+1,n,map,curr+'1',flag);
-    }
     string findDifferentBinaryString(vector<string>& nums) {
-        int n=nums[0].size();
-        unordered_set<string> map(nums.begin(),nums.end());
-        solve(0,n,map,"",false);
+        string ans="";
+        for(int i=0;i<nums.size();i++){
+            ans+=(!(nums[i][i]-'0'))+'0';
+            // ans+= nums[i][i]=='0' ? '1' : '0'; 
+        }
         return ans;
     }
 };
