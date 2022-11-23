4c7c72c86ed9e2f0a4f25b74db62a54117baa38b
Niraj Kumar <86815759+niraj9504@users.noreply.github.com>
2022-11-23 11:30:06 +0530

    Time: 97 ms (83.85%), Space: 43.2 MB (28.62%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,13 +4,14 @@
  * @return {number[]}
  */
 var twoSum = function(nums, t) {
-    const map1={};
+    const map1=new Map();
     for(let i=0;i<nums.length;i++){
         const v=t-nums[i];
-        if(v in map1){
-            return [map1[v],i];
+        if(map1.has(v)){
+            return [map1.get(v),i];
         }
-        map1[nums[i]]=i;
+        // map1[nums[i]]=i;
+        map1.set(nums[i],i);
     }
     return [-1,-1];
 };
