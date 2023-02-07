de5f55b57f8882783e7c0cf163317848d3f56b61
Niraj Kumar <86815759+niraj9504@users.noreply.github.com>
2023-02-07 11:40:29 +0530

    Time: 800 ms (53.67%), Space: 0B (100.00%) - LeetHub

--- 0183-customers-who-never-order/0183-customers-who-never-order.sql
+++ 0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -1,4 +1,6 @@
-select c.name as Customers from Customers c
-left join 
-Orders o on c.id=o.customerId
-where o.customerId is NULL;
+# select c.name as Customers from Customers c
+# left join 
+# Orders o on c.id=o.customerId
+# where o.customerId is NULL;
+select name as 'Customers' from customers 
+where id not in(select customerId from orders);
