7b77c29e07b76d3c17f2871e2348f9dde10f8f7a
Niraj Kumar <86815759+niraj9504@users.noreply.github.com>
2023-08-28 22:32:46 +0530

    Time: 12 ms (49.75%), Space: 5.9 MB (45.78%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string s=to_string(x);
+        int n=s.size();
+        int i=0,j=n-1;
+        while(i<j){
+            if(s[i++]!=s[j--])return false;
+        }
+        return true;
+    }
+};
