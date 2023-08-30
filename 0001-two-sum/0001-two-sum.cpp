5a58022fdc7281e996870b2f9bcbfe54d2cb27ad
Niraj Kumar <86815759+niraj9504@users.noreply.github.com>
2023-08-30 20:57:46 +0530

    Time: 7 ms (92.93%), Space: 10.8 MB (33.34%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,8 +4,10 @@ public:
         int n=nums.size();
         unordered_map<int,int> map;
         for(int i=0;i<n;i++){
-            int x=t-nums[i];
-            if(map.find(x)!=map.end())return {i,map[x]};
+            
+            if(map.find(t-nums[i])!=map.end()){
+                return {map[t-nums[i]],i};
+            }
             map[nums[i]]=i;
         }
         return {};
